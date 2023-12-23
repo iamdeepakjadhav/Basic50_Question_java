171277acdb26d7bd18d80a17526647105f72c200
iamdeepakjadhav <139667385+iamdeepakjadhav@users.noreply.github.com>
2023-12-24 05:25:57 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,17 @@
-public class Fibonacci{
+import java.util.*;
+
+public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println("hello java");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number of fibonacci series : ");
+        int n = sc.nextInt();
+        int a = 0, b = 1, tamp;
+        // System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(a + " ");
+            tamp = a + b;
+            a = b;
+            b = tamp;
+        }
     }
 }

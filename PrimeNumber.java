b1e47577c9ede12d018016d40482c9a688527937
iamdeepakjadhav <139667385+iamdeepakjadhav@users.noreply.github.com>
2023-12-25 09:34:04 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,11 +1,11 @@
 public class PrimeNumber {
     public static void main(String[] args) {
 
-        int num = 9, i = 2;
+        int num = 0, i = 2;
         boolean primeBul = true;
 
         while (i < num) {
-            if (num % i == 0) {
+            if (num % i == 0 || num == 0 || num == 1) {
                 System.out.println(num + " is not a prime");
                 primeBul = false;
                 break;

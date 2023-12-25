e7a450458486e41324c1af0fb83f4a8b64c65a39
iamdeepakjadhav <139667385+iamdeepakjadhav@users.noreply.github.com>
2023-12-25 09:22:32 +0530

    Update PrimeNumber.java
    prime number basic

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -2,18 +2,18 @@ public class PrimeNumber {
     public static void main(String[] args) {
 
         int num = 9, i = 2;
-        boolean primeBul= true;
+        boolean primeBul = true;
 
-        while(i<num){
-            if (num%i==0) {
-                System.out.println(num+" is not a prime");
+        while (i < num) {
+            if (num % i == 0) {
+                System.out.println(num + " is not a prime");
                 primeBul = false;
                 break;
             }
             i++;
         }
-        if(primeBul){
-            System.out.println(num+" is a prime number");
+        if (primeBul) {
+            System.out.println(num + " is a prime number");
         }
     }
 }
